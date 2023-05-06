8f6a93c620dd8ea5745ed6a341e610352c0c1a63
JINGJINGPAN1 <pan_jingjing@foxmail.com>
2023-05-06 13:24:17 +0800

    working on the quicksort

---
+++ QuickSort.java
@@ -0,0 +1,44 @@
+public class QuickSort {
+
+    public static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i+1, high);
+
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {5, 1, 9, 3, 7, 2, 8, 4, 6};
+        int n = arr.length;
+
+        sort(arr, 0, n - 1);
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
