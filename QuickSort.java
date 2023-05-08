e68dda68aef8ecfc4503122873925c70867654ed
Jingjing Pan <pan_jingjing@foxmail.com>
2023-05-08 21:37:04 +0800

    done with QuickSort.

--- QuickSort.java
+++ QuickSort.java
@@ -1,43 +1,43 @@
 public class QuickSort {
-
-    public static void sort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
+    public static void sort(int[] arr){
+        quickSort(arr,0,arr.length-1);
     }
 
-    private static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pivotIndex = partition(arr, low, high);
-            quickSort(arr, low, pivotIndex - 1);
-            quickSort(arr, pivotIndex + 1, high);
+
+    private static void quickSort(int[]arr, int low, int high){
+        if(low < high){
+           int pivotIndex = partition(arr, low, high);
+           quickSort(arr, low,pivotIndex-1);
+           quickSort(arr,pivotIndex+1, high);
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
+
+    public static int partition(int[]arr, int low, int high){
         int pivot = arr[low];
         int i = low + 1;
-
-        for (int j = i; j <= high; j++) {
-            if (arr[j] < pivot) {
+        for(int j = i; j <= high; j++){
+            if(arr[j] <= pivot){
                 swap(arr, i, j);
                 i++;
             }
         }
-
-        swap(arr, low, i - 1);
-        return i - 1;
+        swap(arr, low,i-1);
+        return i-1;
+    }
+    public static void swap(int[]arr, int a , int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
-    private static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    public static void main(String[] args){
+        int[] a = {3,2,1,5,3,6};
+        sort(a);
 
-    public static void main(String[] args) {
-        int[] arr = { 5, 2, 6, 1, 3, 9, 4 };
-        sort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        for(int i = 0; i < a.length; i++){
+            System.out.print(a[i] + " ");
         }
     }
+
 }
