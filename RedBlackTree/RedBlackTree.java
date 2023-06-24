2f1a68cf8ab77ebca71b50ee463ac1cd1f3ed104
Jingjing Pan <pan_jingjing@foxmail.com>
2023-06-24 22:49:31 +0800

    trying on RedBlackTree.

--- RedBlackTree/RedBlackTree.java
+++ RedBlackTree/RedBlackTree.java
@@ -17,80 +17,27 @@ public class RedBlackTree<T extends Comparable<T>> {
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-    /**private Node<T> root;
-
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
-
-    private static class Node<T> {
-        T val;
-        Node<T> left, right;
-        boolean color;
-
-        Node(T val, boolean color) {
-            this.val = val;
-            this.color = color;
+    private Node<T> insert(Node<T> node, T val){
+        if(node == null){
+            return new Node<T>(val,RED);
         }
-    }
-
-    public void insert(T val) {
-        root = insert(root, val);
-        root.color = BLACK;
-    }
-
-    private Node<T> insert(Node<T> node, T val) {
-        if (node == null) {
-            return new Node<T>(val, RED);
-        }
-
         int cmp = val.compareTo(node.val);
-        if (cmp < 0) {
-            node.left = insert(node.left, val);
-        } else if (cmp > 0) {
-            node.right = insert(node.right, val);
+        if(cmp < 0){
+            node.left = insert(node.left,val);
         }
-
-        if (isRed(node.right) && !isRed(node.left)) {
+        if(cmp > 0){
+            node.right = insert(node.right,val);
+        }
+        if(isRed(node.right) && !isRed(node.left)){
             node = rotateLeft(node);
         }
-        if (isRed(node.left) && isRed(node.left.left)) {
+        if(isRed(node.left) && isRed(node.left.left)){
             node = rotateRight(node);
         }
-        if (isRed(node.left) && isRed(node.right)) {
+        if(isRed(node.left) && isRed(node.right)){
             flipColors(node);
         }
-
         return node;
-    }
-
-    private boolean isRed(Node<T> node) {
-        if (node == null) {
-            return false;
-        }
-        return node.color == RED;
     }
 
     private Node<T> rotateLeft(Node<T> h) {
@@ -102,7 +49,7 @@ public class RedBlackTree<T extends Comparable<T>> {
         return x;
     }
 
-    private Node<T> rotateRight(Node<T> h) {
+    private Node<T> rotateRight(Node<T> h){
         Node<T> x = h.left;
         h.left = x.right;
         x.right = h;
@@ -111,9 +58,19 @@ public class RedBlackTree<T extends Comparable<T>> {
         return x;
     }
 
+    private boolean isRed(Node<T> node) {
+        if(node == null){
+            return false;
+        }
+        return node.color == RED;
+
+    }
+
     private void flipColors(Node<T> h) {
         h.color = RED;
         h.left.color = BLACK;
         h.right.color = BLACK;
     }
-}*/
+
+}
+
